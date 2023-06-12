52473ae983995af6958fa74450f1d67a3fcde6f9
yulyazbrv <zubareva.yo@gmail.com>
2023-06-12 14:08:16 +0300

    feat: fix errors

--- server/src/app.js
+++ server/src/app.js
@@ -26,7 +26,7 @@ const { makeKey, checkWinner } = require("./util");
 const { createGame, getGame, updateGame } = require("./data/games");
 const { createPlayer, getPlayer, removePlayer } = require("./data/players");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
   socket.on("disconnect", () => {
